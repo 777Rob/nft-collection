ff61958cbdf7456f302e63336d335daa98b50149
Your Name <you@example.com>
2022-04-08 20:41:28 +0200

    init

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x13E0943ddAB0c2704fa75add1eaBBb62CF09Cb00";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "http://localhost:3000/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
